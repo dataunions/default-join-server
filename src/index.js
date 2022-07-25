ec2b3b211ef42a47dcd3eeb8feb51a1e26e0e3e8
Henri Pihkala <henri@streamr.network>
2022-07-25 18:38:36 +0200

    fix typo

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 const sequelize = new Sequelize(process.env.DB_SCHEMA, process.env.DB_USER, process.env.DB_PASSWORD, {
 	host: process.env.DB_HOST,
-	post: process.env.DB_PORT,
+	port: process.env.DB_PORT,
 	dialect: 'mysql',
 });
 
