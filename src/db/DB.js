f352c30b508e63ed48de76870a80dfe8bf7ddcdb
Henri Pihkala <henri@streamr.network>
2022-07-25 17:51:22 +0200

    add eslint

--- src/db/DB.js
+++ src/db/DB.js
@@ -1,53 +1,53 @@
 const { QueryTypes } = require('sequelize')
-const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid')
 
 class DB {
-    constructor(sequelize) {
-        this.sequelize = sequelize
-    }
+	constructor(sequelize) {
+		this.sequelize = sequelize
+	}
 
-    async getAppSecret(secret) {
-        const result = await this.sequelize.query(
-            'SELECT * FROM data_union_secret WHERE secret = :secret LIMIT 1', {
-                replacements: {
-                    secret,
-                },
-                type: QueryTypes.SELECT
-            }
-        )
-        console.log(result)
-        return result[0]
-    }
+	async getAppSecret(secret) {
+		const result = await this.sequelize.query(
+			'SELECT * FROM data_union_secret WHERE secret = :secret LIMIT 1', {
+				replacements: {
+					secret,
+				},
+				type: QueryTypes.SELECT
+			}
+		)
+		console.log(result)
+		return result[0]
+	}
 
-    async createAppSecret(dataUnionAddress, chain, name) {
-        const secret = uuid()
-        await this.sequelize.query(
-            'INSERT INTO data_union_secret (`secret`, `contract_address`, `chain`, `name`) VALUES (:secret, :dataUnionAddress, :chain, :name)',
-            {
-                replacements: {
-                    secret,
-                    dataUnionAddress,
-                    chain,
-                    name,
-                },
-                type: QueryTypes.INSERT
-            }
-        )
+	async createAppSecret(dataUnionAddress, chain, name) {
+		const secret = uuid()
+		await this.sequelize.query(
+			'INSERT INTO data_union_secret (`secret`, `contract_address`, `chain`, `name`) VALUES (:secret, :dataUnionAddress, :chain, :name)',
+			{
+				replacements: {
+					secret,
+					dataUnionAddress,
+					chain,
+					name,
+				},
+				type: QueryTypes.INSERT
+			}
+		)
 
-        return this.getAppSecret(secret)
-    }
+		return this.getAppSecret(secret)
+	}
 
-    async deleteAppSecret(secret) {
-        const result = await this.sequelize.query(
-            'DELETE FROM data_union_secret WHERE secret = :secret', {
-                replacements: {
-                    secret,
-                },
-                type: QueryTypes.DELETE
-            }
-        )
-        console.log(result)
-    }
+	async deleteAppSecret(secret) {
+		const result = await this.sequelize.query(
+			'DELETE FROM data_union_secret WHERE secret = :secret', {
+				replacements: {
+					secret,
+				},
+				type: QueryTypes.DELETE
+			}
+		)
+		console.log(result)
+	}
 
 }
 
--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ const { Sequelize } = require('sequelize')
 require('dotenv').config()
 
 const sequelize = new Sequelize(process.env.DB_SCHEMA, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    post: process.env.DB_PORT,
-    dialect: 'mysql',
-})
+	host: process.env.DB_HOST,
+	post: process.env.DB_PORT,
+	dialect: 'mysql',
+});
 
 (async () => {
-    try {
-        await sequelize.authenticate()
-        console.log('Database connection established successfully.')
-      } catch (error) {
-        console.error('Unable to connect to the database:', error)
-        process.exit(1)
-      }
+	try {
+		await sequelize.authenticate()
+		console.log('Database connection established successfully.')
+	} catch (error) {
+		console.error('Unable to connect to the database:', error)
+		process.exit(1)
+	}
 
 
 })()
