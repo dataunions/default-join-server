c00a296f0af8062062f84d1c6338604acebe1643
Henri Pihkala <henri@streamr.network>
2022-07-26 13:06:23 +0200

    tests for custom routes

--- src/CustomRoutes.js
+++ src/CustomRoutes.js
@@ -1,23 +1,31 @@
 module.exports = (client, db) => {
 	return (expressApp) => {
-		
+
 		// Owner authenticator middleware for the secrets management routes
-		expressApp.use('/secrets/*', async (req, res, next) => {
+		expressApp.use('/secrets/', async (req, res, next) => {
 			const dataUnion = await client.getDataUnion(req.validatedRequest.dataUnion)
-			const owner = await dataUnion.getOwner()
-			if (owner.toLowerCase() === req.body.address.toLowerCase()) {
-				next()
-			} else {
-				res.status(403)
+			if (!dataUnion) {
+				res.status(404)
 				res.set('content-type', 'application/json')
 				res.send({
-					error: `This endpoint can only be called by the Data Union owner (${owner})`
+					error: `Data Union ${req.validatedRequest.dataUnion} on chain ${req.validatedRequest.chain} does not exist!`
 				})
+			} else {
+				const owner = await dataUnion.getOwner()
+				if (owner.toLowerCase() === req.body.address.toLowerCase()) {
+					next()
+				} else {
+					res.status(403)
+					res.set('content-type', 'application/json')
+					res.send({
+						error: `This endpoint can only be called by the Data Union owner (${owner})`
+					})
+				}
 			}
 		})
 
 		expressApp.post('/secrets/list', async (req, res) => {
-		// Get secrets from DB
+			// Get secrets from DB
 			const secrets = await db.listSecrets(req.validatedRequest.dataUnion, req.validatedRequest.chain)
 
 			res.status(200)
@@ -26,7 +34,7 @@ module.exports = (client, db) => {
 		})
 
 		expressApp.post('/secrets/create', async (req, res) => {
-		// Insert new secret to DB
+			// Insert new secret to DB
 			const secret = await db.createAppSecret(req.validatedRequest.dataUnion, req.validatedRequest.chain, req.validatedRequest.name)
 
 			res.status(200)
@@ -38,7 +46,7 @@ module.exports = (client, db) => {
 			const secret = await db.getAppSecret(req.validatedRequest.secret)
 
 			if (secret) {
-			// Delete secret
+				// Delete secret
 				await db.deleteAppSecret(secret.secret)
 
 				res.status(200)
